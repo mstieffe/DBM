a71c5e0d5f18c39e894c46fa3247ae9ce02d071b
mstieffe <mstieffe@students.uni-mainz.de>
2023-04-13 14:32:30 +0200

    tqdm

--- dbm/gan_seq.py
+++ dbm/gan_seq.py
@@ -8,7 +8,7 @@ from timeit import default_timer as timer
 from dbm.util import rot_mtx_batch, make_dir, avg_blob, make_grid_np, transpose_and_zip, transpose, insert_dim, repeat, to_voxel
 from dbm.output import OutputHandler
 #from tqdm.autonotebook import tqdm
-from tqdm import tqdm
+from tqdm.auto import tqdm
 import dbm.model as model
 from dbm.data import Data
 from dbm.histogram import Histogram
